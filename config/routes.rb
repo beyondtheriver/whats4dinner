686c0481364b0dfcbf0401a60c750d1963b3ee64
Rick Mitchell <rickbmitchell@gmail.com]>
2016-03-30 10:28:13 -0400

    dynamic search form, fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,22 @@ Rails.application.routes.draw do
 devise_scope :user do
     get '/users/auth/:provider/callback' => 'omniauth_callbacks#test'
 end
-  
 
-  
+
+
   get 'recipes/individual_recipe/:id' => 'recipes#individual_recipe'
 
   get '/recipes/show' => 'recipes#show'
 
   post 'ingredient_search' => 'recipes#show'
 
+  get 'recipes/index'
 
+  get 'static_pages/home'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#home'
 
 
   resources :profiles
