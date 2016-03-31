822befb5c09428638ac19280d06eabd6f7206635
Rick Mitchell <rickbmitchell@gmail.com]>
2016-03-31 17:28:36 -0400

    ajax thingy

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 require 'unirest'
 class RecipesController < ApplicationController
 
-
   def index
 
   end
@@ -18,18 +17,16 @@ class RecipesController < ApplicationController
 
     ingredientssearch = params[:ingredients].join(",")
 
-
      cookies[:list_of_recipes] = new_API_first_call(ingredientssearch)
-   
-    
+
     # @recipe = Recipe.find(params[:id])
-    
+
   end
 
   def individual_recipe
     # the params[:id] here is actually the API id 6 digit
     if (Recipe.where("api_id = ?", params[:id]) == true)
-      
+
 
       @recipe = Recipe.where(api_id: params[:id]).take
       @image = @recipe.image
@@ -40,7 +37,7 @@ class RecipesController < ApplicationController
 
       @resp = second_API_call_one_recipe(params[:id].to_s)
        @cookievalue = JSON.parse(cookies[:list_of_recipes])
-       
+
        @cookievalue.each do |key|
           if key["id"] == params[:id].to_i
             @id = key["id"]
@@ -48,20 +45,20 @@ class RecipesController < ApplicationController
             @image = key["image"]
           end
         end
-       
+
         @recipe = Recipe.create(:api_id => @id, :title => @title, :image => @image, :vegetarian => @resp.body['vegetarian'], :vegan => @resp.body['vegan'], :gluten_free => @resp.body['glutenFree'], :dairy_free => @resp.body['dairyFree'], :instructions => @resp.body['instructions'])
 
         @extended_ing = []
         @resp.body["extendedIngredients"].each do |key|
-           x = ExtendedIngredient.create(:original_string =>  key["originalString"] , :name => key["name"], :recipe_id => @recipe.id)      
+           x = ExtendedIngredient.create(:original_string =>  key["originalString"] , :name => key["name"], :recipe_id => @recipe.id)
            @extended_ing.push(x)
            Ingredient.create(:name => key["name"])
-         end 
+         end
 
     end
 
 
-      
+
   end
 
   def new_API_first_call(ingredientssearch)
