5ca888ecd971ba23b050c93693ae7ed168ce5bf3
Rick Mitchell <rickbmitchell@gmail.com]>
2016-04-08 12:18:10 -0400

    default photo and layout tweaks

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_attached_file :avatar, :styles =>
    { :medium => "300x300>", :thumb => "100x100>" },
-   :default_url => "/images/:style/missing.png"
+   :default_url => "missing.png", id: "avatar"
    validates_attachment_content_type :avatar,
    content_type: /\Aimage\/.*\Z/
 
